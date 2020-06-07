19401ab8c569c91298bb638a353465324217ca69
huyhieu <lehuyhieu1099@gmail.com>
2020-06-07 14:49:38 +0700

    Cau truc dieu kien 2

---
+++ Bài tập james/sự kiện/maytinh.js
@@ -0,0 +1,26 @@
+
+function add() {
+    a = parseInt(document.getElementById('so1').value) + parseInt(document.getElementById('so2').value)
+    document.getElementById('result').value = a;
+}
+function sub() {
+    s = parseInt(document.getElementById('so1').value) - parseInt(document.getElementById('so2').value)
+    document.getElementById('result').value = s;
+}
+function mul() {
+    m = parseInt(document.getElementById('so1').value) * parseInt(document.getElementById('so2').value)
+    document.getElementById('result').value = m;
+}
+function div() {
+    d = parseInt(document.getElementById('so1').value) / parseInt(document.getElementById('so2').value)
+    document.getElementById('result').value = d;
+}
+
+
+
+
+
+
+
+
+
